63e10939b1be9c296acc376da3470692473bd8f6
Justin Gallagher <59921901+JustinG512@users.noreply.github.com>
2023-04-11 13:21:32 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
     public static void main(String []args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
         int month = 1;
         int day = 2;
